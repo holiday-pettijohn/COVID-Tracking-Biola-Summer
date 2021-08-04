eafb0506fd3168ad531b8e165bdb04e58c9f7b11
danieltmarch <43455439+danieltmarch@users.noreply.github.com>
2021-08-04 09:39:42 -0700

    IRDK and other stuffs

--- Code/Models/IRDK.py
+++ Code/Models/IRDK.py
@@ -4,46 +4,72 @@ import random
 
 #min: (y-f(theta))^2
 #y is the infections, and f is the function to simulate infections given the parameters
-#theta = [I(0), A(0), q, gamma0, gamma1, kappa, beta0, beta1, beta2, beta3]
-# A' = (b0 + b1 / (1 + (b2*I)^b3 ) ) * (q*Pop * A) / (q*Pop + A) - kappa*A - gamma0*A
-# I' = kappa*A - gamma1*I
 
-#A' = A(t+1) - A(t)
+#theta = [I(0), A(0), q, alpha0, alpha1, beta0, beta1, beta2]
+
+# I' = (b0 + b1 / (1 + (K)^b2 ) ) * (q*Pop * I) / (q*Pop + I) - R' - D'
+# K' = alpha0I - alpha1*K #known public perception of the virus
+#K(0) = 0
+
+#you don't need a scaler on K since it is arbitrary, this will e adjusted by alpha0
 
 #note that I should be modified so pop = 1
-class AIRD:
+class IRDK:
     actualI = np.zeros(0) #empty numpy array until set
+    deathRate = 0
+    recovRate = 0
+    
     theta = np.zeros(0) #the current variables, when solving this is set at the end
     I = np.zeros(0) #simulated infections
-    A = np.zeros(0) #simulated asymptomatics
+    K = np.zeros(0) #simulated known
     
-    def setInfections(self, infect, pop = 1): #define the infections for the model, if pop is set, infections will be scaled
-        self.actualI = infect / pop
+    
+    def setRecovRate(self, I, R, D):
+        #R' = gamma0*I
+        #D' = gamma1*I
+        
+        y = np.zeros((len(I)-1,1))
+        x = np.zeros((len(I)-1,1))
+
+        # R(t+1) - R(t) = gamma*I(t)
+        y[:,0] = R[1:] - R[:-1]
+        x[:,0] = I[:-1]
+        
+        self.recovRate = np.linalg.lstsq(x, y, rcond = None)[0].flatten()[0] #solve for gamma
+        
+        # D(t+1) - D(t) = *I(t)
+        y[:,0] = D[1:] - D[:-1]
+        #x is already set correctly
+        
+        self.deathRate = np.linalg.lstsq(x, y, rcond = None)[0].flatten()[0] #solve for gamma
+        
+    
+    def setInfections(self, I, pop = 1): #define the infections for the model, if pop is set, infections will be scaled
+        self.actualI = I / pop
 
     #simulate the number of infections given the variables theta
     def simulate(self, theta, setSelf=False):
         I = np.zeros(np.shape(self.actualI))
-        A = np.zeros(np.shape(self.actualI))
+        K = np.zeros(np.shape(self.actualI))
 
         I[0] = theta[0] # I(0)
-        A[0] = theta[1] # A(0)
+        K[0] = theta[1]
         q = theta[2]
-        gamma0 = theta[3]
-        gamma1 = theta[4]
-        kappa = theta[5]
-        beta = theta[6:] #beta0, beta1, beta2, beta3
+        alpha0 = theta[3]
+        alpha1 = theta[4]
+        beta = theta[5:] #beta0, beta1, beta2
 
-        #iterate the arrays using the definition A' and I'
-        for t in range(len(I)-1): #define I and A on range [1, length)
-            diffA = (beta[0] + (beta[1] / (1 + (beta[2]*I[t])**beta[3] )) ) * (q*A[t])/(q + A[t]) - kappa*A[t] - gamma0*A[t]
-            diffI = kappa*A[t] - gamma1*I[t]
+        #iterate the arrays using the definition K' and I'
+        for t in range(len(I)-1): #define I and K on range [1, length)
+            diffI = (beta[0] + (beta[1] / (1 + (K[t])**beta[2] )) ) * (q*I[t])/(q + I[t]) - (self.recovRate + self.deathRate)*I[t]
+            diffK = alpha0*I[t] - alpha1*K[t]
 
-            A[t+1] = diffA + A[t]
             I[t+1] = diffI + I[t]
+            K[t+1] = diffK + K[t]
             
         if(setSelf):
             self.I = I
-            self.A = A
+            self.K = K
         
         return I
 
@@ -117,21 +143,18 @@ class AIRD:
     
     def checkRanges(self, theta): #make sure all the variables are within range
         theta[0] = self.boundVariable(theta[0], 0, .005) #I(t)
-        theta[1] = self.boundVariable(theta[1], 0.000001, .005) #A(t)
+        theta[1] = self.boundVariable(theta[1], 0.000001, 10e25) #K(t)
         
         theta[2] = self.boundVariable(theta[2], .00001, 1) #q
         
-        theta[3] = self.boundVariable(theta[3], 0.00001, .1) #gamma0
-        theta[4] = self.boundVariable(theta[4], 0.00001, .1) #gamma1
+        theta[3] = self.boundVariable(theta[3], 0.00001, 10e10) #alpha0
+        theta[4] = self.boundVariable(theta[4], 0.00001, 1) #alpha1
         
-        theta[5] = self.boundVariable(theta[5], .00001, .3) #kappa
-        
-        theta[6] = self.boundVariable(theta[6], 0.00001, theta[5] + theta[3]) #beta0 < gamma0 + kappa
-        #theta[6] = 0
-        #theta[7] = self.boundVariable(theta[7], 0.00001, 5) #beta1
-        theta[7] = self.boundVariable(theta[7], theta[5] + theta[3] - theta[6], 5)
-        theta[8] = self.boundVariable(theta[8], 0, 1e10) #beta2
-        theta[9] = self.boundVariable(theta[9], .5, 10) #beta3
+        theta[5] = self.boundVariable(theta[5], 0.0000001, self.recovRate + self.deathRate) #beta0 < recoveries
+        #theta[5] = 0
+        #theta[6] = self.boundVariable(theta[6], 0.00001, 5) #beta1
+        theta[6] = self.boundVariable(theta[6], self.recovRate + self.deathRate - theta[6], 5)
+        theta[7] = self.boundVariable(theta[7], .25, 10) #beta3
         
         return theta
         
@@ -139,22 +162,20 @@ class AIRD:
         
     
     def getRandomInit(self): #random starting conditions
-        theta = np.zeros(10) #10 vars to solve
+        theta = np.zeros(8) #8 vars to solve
         
         theta[0] = random.random()*.001 #I(0), between 0 and .1 percent of the population
-        theta[1] = random.random()*.003 #A(0), between 0 and .3 percent of the population
+        theta[1] = random.random()*.1 #A(0), between 0 and 10 percent of the population
         
-        theta[2] = .01 + random.random()*.99 #q: [.01, 1]
+        theta[2] = .1 + random.random()*.9 #q: [.1, 1]
         
-        theta[3] = .001 + random.random()*.1 #gamma0 asympt recov rate [.001, .101]
-        theta[4] = .001 + random.random()*.1 #gamma1 infect recov rate [.001, .101]
+        theta[3] = .1 + random.random()*10 #alpha0 knowledge rate [.001, 10.1]
+        theta[4] = .001 + random.random()*.05 #alpha1 knowledge decay rate [.001, .051]
         
-        theta[5] = .01 + random.random()*.2 #kappa, asypt -> infect rate [.01, .21]
-        
-        theta[6] = .001 + random.random()*.2 #beta0, floor infection rate [.001, .201]
-        theta[7] = (theta[5] + theta[3] - theta[6]) * (.5*random.random() + 1) #beta1, ceiling trans rate, 1 to 1.5 range
-        theta[8] = 1 + random.random()*500 #beta2, scaler to I [1, 501]
-        theta[9] = .5 + random.random()*5 #exponential for I*beta2 [.5, 5.5]
+        theta[5] = (self.recovRate+self.deathRate) * random.random() #beta0, floor infection rate [.001, .201]
+        #theta[5] = 0
+        theta[6] = (self.recovRate+self.deathRate - theta[5]) * (.5*random.random() + 1) #beta1, ceiling trans rate, 1 to 1.5 range
+        theta[7] = .5 + random.random()*5 #beta2, exponential [.5, 5.5]
         
         return theta
     
@@ -207,18 +228,19 @@ class AIRD:
     def printTheta(self, theta):
         print("I(0), A(0):", theta[0], theta[1])
         print("q:", theta[2])
-        print("gamma:", theta[3:5])
-        print("kappa:", theta[5])
-        print("beta:", theta[6:])
+        print("alpha:", theta[3:5])
+        print("beta:", theta[5:])
         print() #spacer
     
-    def graph(self, graphA = False):
+    def graph(self, graphK = False):
         fig, ax = plt.subplots(figsize=(18,8))
         ax.plot(self.actualI, color="red")
         ax.plot(self.I, color="red", linestyle="dashed")
         
-        if(graphA):
-            ax.plot(self.A, color="orange", linestyle="dashed")
-        
+        if(graphK):
+            scaleFactor = max(self.actualI) / max(self.K)
+            print("K scaled by:", scaleFactor)
+            ax.plot(self.K*scaleFactor, color="orange", linestyle="dashed")
+     
         return fig, ax
         
